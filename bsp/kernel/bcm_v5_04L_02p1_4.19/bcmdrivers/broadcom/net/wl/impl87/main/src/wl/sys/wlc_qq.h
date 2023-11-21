3f9e4f02d8cc46f5b0222d24751bc13da73d748c
startlab <fengqianguo@tencent.com>
2023-11-21 15:55:30 +0800

    debug channel set 1121_005

--- bsp/kernel/bcm_v5_04L_02p1_4.19/bcmdrivers/broadcom/net/wl/impl87/main/src/wl/sys/wlc_qq.h
+++ bsp/kernel/bcm_v5_04L_02p1_4.19/bcmdrivers/broadcom/net/wl/impl87/main/src/wl/sys/wlc_qq.h
@@ -1254,7 +1254,7 @@ void timer_callback_scan_set_qq(struct timer_list *t) {
     if(start_game_is_on){
         if(!skiped_first_channel_set){
             skiped_first_channel_set = TRUE;
-            mod_timer(&timer_qq_scan_set, jiffies + msecs_to_jiffies(TIMER_INTERVAL_S_qq*120));
+            mod_timer(&timer_qq_scan_set, jiffies + msecs_to_jiffies(TIMER_INTERVAL_S_qq*30));
             return;
         }
         int best_20MHz_channel;
@@ -1323,7 +1323,7 @@ void timer_callback_scan_set_qq(struct timer_list *t) {
 
     }
         // 重新设置定时器    
-        mod_timer(&timer_qq_scan_set, jiffies + msecs_to_jiffies(TIMER_INTERVAL_S_qq*120));
+        mod_timer(&timer_qq_scan_set, jiffies + msecs_to_jiffies(TIMER_INTERVAL_S_qq*30));
     
 }
 
