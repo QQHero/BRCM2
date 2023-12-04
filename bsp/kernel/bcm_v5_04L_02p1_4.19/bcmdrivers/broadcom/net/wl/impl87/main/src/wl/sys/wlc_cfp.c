94e4740106aa8d0f12979f910f9f0f6e45743c12
startlab <fengqianguo@tencent.com>
2023-12-04 10:28:42 +0800

    debug chan set 1204_001(add rssi data type)

--- bsp/kernel/bcm_v5_04L_02p1_4.19/bcmdrivers/broadcom/net/wl/impl87/main/src/wl/sys/wlc_cfp.c
+++ bsp/kernel/bcm_v5_04L_02p1_4.19/bcmdrivers/broadcom/net/wl/impl87/main/src/wl/sys/wlc_cfp.c
@@ -2609,6 +2609,7 @@ wlc_cfp_scb_chain_sendup(wlc_info_t *wlc, scb_cfp_t * scb_cfp, uint8 prio)
 							phy_info_qq_cur->noiselevel = wlc_lq_chanim_phy_noise(wlc);
 							phy_info_qq_cur->RSSI = phy_info_qq_rx_new.RSSI;
 							phy_info_qq_cur->RSSI_loc = 0;
+							phy_info_qq_cur->RSSI_type = FC_TYPE(fc_qq);
 							memcpy(info_qq, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
 							debugfs_set_info_qq(2, info_qq, 1);
 							MFREE(wlc->osh, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
@@ -2620,6 +2621,7 @@ wlc_cfp_scb_chain_sendup(wlc_info_t *wlc, scb_cfp_t * scb_cfp, uint8 prio)
 							phy_info_qq_cur->noiselevel = wlc_lq_chanim_phy_noise(wlc);
 							phy_info_qq_cur->RSSI = wlc_lq_rssi_get(wlc, SCB_BSSCFG(scb), scb);
 							phy_info_qq_cur->RSSI_loc = 3;
+							phy_info_qq_cur->RSSI_type = FC_TYPE(fc_qq);
 							memcpy(info_qq, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
 							debugfs_set_info_qq(2, info_qq, 1);
 							MFREE(wlc->osh, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
@@ -2632,6 +2634,7 @@ wlc_cfp_scb_chain_sendup(wlc_info_t *wlc, scb_cfp_t * scb_cfp, uint8 prio)
 							phy_info_qq_cur->noiselevel = wlc_lq_chanim_phy_noise(wlc);
 							phy_info_qq_cur->RSSI = wrxh->rssi;
 							phy_info_qq_cur->RSSI_loc = 4;
+							phy_info_qq_cur->RSSI_type = FC_TYPE(fc_qq);
 							memcpy(info_qq, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
 							debugfs_set_info_qq(2, info_qq, 1);
 							MFREE(wlc->osh, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
--- bsp/kernel/bcm_v5_04L_02p1_4.19/bcmdrivers/broadcom/net/wl/impl87/main/src/wl/sys/wlc_rx.c
+++ bsp/kernel/bcm_v5_04L_02p1_4.19/bcmdrivers/broadcom/net/wl/impl87/main/src/wl/sys/wlc_rx.c
@@ -747,6 +747,7 @@ wlc_recv(wlc_info_t *wlc, void *p)
                         phy_info_qq_cur->noiselevel = wlc_lq_chanim_phy_noise(wlc);
                         phy_info_qq_cur->RSSI = phy_info_qq_rx_new.RSSI;
 						phy_info_qq_cur->RSSI_loc = 2;
+                        phy_info_qq_cur->RSSI_type = FC_TYPE(fc_qq);
                         memcpy(info_qq, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
                         debugfs_set_info_qq(2, info_qq, 1);
                         MFREE(wlc->osh, phy_info_qq_cur, sizeof(*phy_info_qq_cur));
